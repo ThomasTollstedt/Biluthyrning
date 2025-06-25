a92fea9d3902f8f7f2c3beeeac3f9c26f7067363
Thomas Tollstedt <thomastollstedt@gmail.com>
2025-06-25 08:57:51 +0200

    Added customer.cs

---
+++ Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjektCarRental.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
